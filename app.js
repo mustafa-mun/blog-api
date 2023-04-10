8a21e88d78838fabcb904f02998e406f021ad525
Mustafa <mustafamcm9@gmail.com>
2023-04-11 00:27:22 +0300

    skeleton files

---
+++ app.js
@@ -0,0 +1,44 @@
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+require("dotenv").config();
+
+const indexRouter = require("./routes/index");
+
+const app = express();
+
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const mongoDB = process.env.MONGODB_URI || process.env.LOCAL_URI;
+
+async function mongooseConnect() {
+  await mongoose.connect(mongoDB);
+}
+mongooseConnect().catch((err) => console.log(err));
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // send error status
+  res.sendStatus(err.status || 500);
+});
+
+module.exports = app;
