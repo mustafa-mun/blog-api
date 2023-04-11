b13bc70383476f0390ba3cdc10db51c377a523a1
Mustafa <mustafamcm9@gmail.com>
2023-04-11 18:11:55 +0300

    users route bug fix

--- routes/users.js
+++ routes/users.js
@@ -10,8 +10,8 @@ router.get("/", userController.users_get);
 // POST REQUEST
 router.post("/", userController.users_post);
 // PUT REQUEST
-router.put("/:postId", userController.users_update);
+router.put("/:userId", userController.users_update);
 // DELETE REQUEST
-router.delete("/:postId", userController.users_delete);
+router.delete("/:userId", userController.users_delete);
 
 module.exports = router;
