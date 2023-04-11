f8363e4ddb62cbce761ff3762a64442242fee092
Mustafa <mustafamcm9@gmail.com>
2023-04-11 15:44:54 +0300

    home page api routes message

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json({ message: "Welcome to blog api" })
+router.get("/", function (req, res, next) {
+  res.json({
+    message: "Welcome to blog api",
+    endpoints: {
+      users: {
+        get: "/users",
+        post: "/users",
+        put: "/users/:userId",
+        delete: "/users/:userId",
+      },
+      posts: {
+        get: "/posts",
+        post: "/posts",
+        put: "/posts/:userId",
+        delete: "/posts/:userId",
+      },
+      comments: {
+        get: "/comments",
+        post: "/comments",
+        put: "/comments/:userId",
+        delete: "/comments/:userId",
+      },
+    },
+  });
 });
 
 module.exports = router;
