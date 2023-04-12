f76e02a7a5b3aa97cfbcbaa3a6f2c61cd3014fcf
Mustafa <mustafamcm9@gmail.com>
2023-04-12 17:34:23 +0300

    add protected routes   This commit protects create, update and delete routes on posts and users route.

--- routes/posts.js
+++ routes/posts.js
@@ -3,30 +3,48 @@ const router = express.Router();
 
 const postsController = require("../controllers/postsController");
 const commentsController = require("../controllers/commentsController");
+const authController = require("../controllers/authController");
 
 /* /posts ROUTES */
 
 // GET REQUEST
 router.get("/", postsController.posts_get);
-// POST REQUEST
-router.post("/", postsController.posts_post);
-// PUT REQUEST
-router.put("/:postId", postsController.posts_update);
-// DELETE REQUEST
-router.delete("/:postId", postsController.posts_delete);
+// POST REQUEST (PROTECTED ROUTE)
+router.post("/", authController.authenticateToken, postsController.posts_post);
+// PUT REQUEST (PROTECTED ROUTE)
+router.put(
+  "/:postId",
+  authController.authenticateToken,
+  postsController.posts_update
+);
+// DELETE REQUEST (PROTECTED ROUTE)
+router.delete(
+  "/:postId",
+  authController.authenticateToken,
+  postsController.posts_delete
+);
 
 /* /comments ROUTES */
 
 // GET REQUEST
 router.get("/comments/", commentsController.all_comments_get);
 router.get("/:postId/comments/", commentsController.comments_get);
-// POST REQUEST
-router.post("/:postId/comments/", commentsController.comments_post);
-// PUT REQUEST
-router.put("/:postId/comments/:commentId", commentsController.comments_update);
-// DELETE REQUEST
+// POST REQUEST (PROTECTED ROUTE)
+router.post(
+  "/:postId/comments/",
+  authController.authenticateToken,
+  commentsController.comments_post
+);
+// PUT REQUEST (PROTECTED ROUTE)
+router.put(
+  "/:postId/comments/:commentId",
+  authController.authenticateToken,
+  commentsController.comments_update
+);
+// DELETE REQUEST (PROTECTED ROUTE)
 router.delete(
   "/:postId/comments/:commentId",
+  authController.authenticateToken,
   commentsController.comments_delete
 );
 
--- routes/users.js
+++ routes/users.js
@@ -2,16 +2,31 @@ const express = require("express");
 const router = express.Router();
 
 const userController = require("../controllers/userController");
+const authController = require("../controllers/authController");
 
 /* /users ROUTES */
 
 // GET REQUEST
 router.get("/", userController.users_get);
+// GET REQUEST
+router.get("/signup", authController.get_signup);
 // POST REQUEST
-router.post("/", userController.users_post);
-// PUT REQUEST
-router.put("/:userId", userController.users_update);
-// DELETE REQUEST
-router.delete("/:userId", userController.users_delete);
+router.post("/signup", authController.post_signup);
+// GET REQUEST
+router.get("/login", authController.get_login);
+// POST REQUEST
+router.post("/login", authController.post_login);
+// PUT REQUEST (PROTECTED ROUTE)
+router.put(
+  "/:userId",
+  authController.authenticateToken,
+  userController.users_update
+);
+// DELETE REQUEST (PROTECTED ROUTE)
+router.delete(
+  "/:userId",
+  authController.authenticateToken,
+  userController.users_delete
+);
 
 module.exports = router;
