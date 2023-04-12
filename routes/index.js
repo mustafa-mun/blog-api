67110fe3ba0baca6d1af73d3461429e8e7a8b207
Mustafa <mustafamcm9@gmail.com>
2023-04-12 19:13:34 +0300

    update home page   Update api explanation json on home page and send it after formatting.

--- routes/index.js
+++ routes/index.js
@@ -3,60 +3,107 @@ const router = express.Router();
 
 /* API explanation. */
 router.get("/", function (req, res, next) {
-  res.json({
-    message: "Welcome to blog api",
-    endpoints: {
-      users: {
-        get: {
-          endpoint: "/users",
-          explanation: "get all users",
+  const apiDoc = {
+    "message": "Welcome to my blog api. You can find the endpoints you can use below.",
+    "endpoints": {
+      "users": {
+        "get": {
+          "endpoint": "/users",
+          "explanation": "get all users",
+          "protected_route": false
         },
-        post: {
-          endpoint: "/users",
-          explanation: "create new user",
+        "get": {
+          "endpoint": "/users/signup",
+          "explanation": "get signup",
+          "note": "this endpoint needs to be connected with a frontend, it's currenty only available on development env",
+          "protected_route": false
         },
-        put: {
-          endpoint: "/users/:userId",
-          explanation: "update user",
+        "post": {
+          "endpoint": "/users/signup",
+          "explanation": "register a new user",
+          "protected_route": false
         },
-        delete: {
-          endpoint: "/users/:userId",
-          explanation: "delete user",
+        "get": {
+          "endpoint": "/users/login",
+          "explanation": "get login",
+          "note": "this endpoint needs to be connected with a frontend, it's currenty only available on development env",
+          "protected_route": false
         },
+        "post": {
+          "endpoint": "/users/login",
+          "explanation": "login",
+          "protected_route": false
+        },
+        "post": {
+          "endpoint": "/users/logout",
+          "explanation": "logout",
+          "protected_route": false
+        },
+        "put": {
+          "endpoint": "/users/:userId",
+          "explanation": "update user",
+          "protected_route": true
+        },
+        "delete": {
+          "endpoint": "/users/:userId",
+          "explanation": "delete user",
+          "protected_route": true
+        }
       },
-      posts: {
-        get: {
-          endpoint: "/posts",
-          explanation: "get all posts",
+      "posts": {
+        "get": {
+          "endpoint": "/posts",
+          "explanation": "get all posts",
+          "protected_route": false
         },
-        post: {
-          endpoint: "/posts",
-          explanation: "create new post",
+        "post": {
+          "endpoint": "/posts",
+          "explanation": "create new post",
+          "protected_route": true
         },
-        put: {
-          endpoint: "/posts/:postId",
-          explanation: "update post",
+        "put": {
+          "endpoint": "/posts/:postId",
+          "explanation": "update post",
+          "protected_route": true
         },
-        delete: {
-          endpoint: "/posts/:postId",
-          explanation: "delete post",
-        },
+        "delete": {
+          "endpoint": "/posts/:postId",
+          "explanation": "delete post",
+          "protected_route": true
+        }
       },
-      comments: {
-        get: {
-          endpoint: "/posts/comments",
-          explanation: "get all comments",
+      "comments": {
+        "get": {
+          "endpoint": "/posts/comments",
+          "explanation": "get all comments",
+          "protected_route": false
         },
-        get: {
-          endpoint: "/posts/:postId/comments",
-          explanation: "get comments on spesific post",
+        "get": {
+          "endpoint": "/posts/:postId/comments",
+          "explanation": "get comments on spesific post",
+          "protected_route": false
         },
-        post: "/posts/comments",
-        put: "/posts/comments/:userId",
-        delete: "/posts/comments/:userId",
-      },
-    },
-  });
+        "post": {
+          "endpoint": "/posts/:postId/comments",
+          "explanation": "add a new comment to a spesific post",
+          "protected_route": true
+        },
+        "put": {
+          "endpoint": "/posts/:postId/comments/:userId",
+          "explanation": "update a comment on a spesific post",
+          "protected_route": true
+        },
+        "delete": {
+          "endpoint": "/posts/:postId/comments/:userId",
+          "explanation": "delete a comment on a spesific post",
+          "protected_route": true
+        }
+      }
+    }
+  }
+  
+  const prettyJSON = JSON.stringify(apiDoc, null, 2); // 2 spaces for indentation
+  res.type('json').send(prettyJSON);
 });
 
 module.exports = router;
