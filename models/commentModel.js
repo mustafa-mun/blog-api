d5116bc9e5f5699565c07df52f6f1da0ddab1bd5
Mustafa <mustafamcm9@gmail.com>
2023-04-13 19:39:05 +0300

    update model   This commit updates comment model. Comment model now requires name and  mail fields.

--- models/commentModel.js
+++ models/commentModel.js
@@ -2,16 +2,13 @@ const mongoose = require("mongoose");
 const Schmea = mongoose.Schema;
 
 const CommentsSchema = new Schmea({
-  post_id: { type: Schmea.Types.ObjectId, required: true },
-  author: { type: String, required: true },
+  post: { type: Schmea.Types.ObjectId, required: true },
+  name: { type: String, required: true },
+  mail: { type: String, required: true },
   content: { type: String, required: true },
   created_at: { type: Date, default: Date.now },
   updated_at: { type: Date, default: Date.now },
   likes: { type: Number, default: 0 },
-});
-
-CommentsSchema.virtual("url").get(function () {
-  return `/posts/${this.post_id}/comments/${this._id}`;
 });
 
 module.exports = new mongoose.model("Comments", CommentsSchema);
